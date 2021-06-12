14a25b38bf5ee4e55774153557132e8ebf7854c4
AviusX <aviusanima@gmail.com>
2021-06-12 14:23:21 +0530

    Fixed rounded button styles in production.

--- src/components/Buttons/RoundedButton.tsx
+++ src/components/Buttons/RoundedButton.tsx
@@ -2,12 +2,27 @@ interface Props {
     color: "primary" | "accent";
 }
 
-const RoundedButton: React.FC<Props> = props => (
-    <button className={`py-2 px-4 md:py-3 md:px-5 xl:px-7 border-2 border-${props.color} font-semibold focus:outline-none
-    text-${props.color} rounded-full hover:bg-${props.color} hover:text-black transition-color duration-300`}
-    >
-        {props.children}
-    </button>
-);
+const RoundedButton: React.FC<Props> = props => {
+    let colorClasses;
+
+    // It is important that the entire tailwind classname is used instead of string
+    // concatenation to stop tailwind production optimization from removing it.
+    // More info here-
+    // https://tailwindcss.com/docs/optimizing-for-production#writing-purgeable-html
+
+    if (props.color === "primary") {
+        colorClasses = "border-primary text-primary hover:bg-primary";
+    } else if (props.color === "accent") {
+        colorClasses = "border-accent text-accent hover:bg-accent";
+    }
+
+    return (
+        <button className={`py-2 px-4 md:py-3 md:px-5 xl:px-7 border-2 font-semibold focus:outline-none
+        rounded-full hover:text-black transition-color duration-300 ${colorClasses}`}
+        >
+            {props.children}
+        </button>
+    );
+}
 
 export default RoundedButton;
